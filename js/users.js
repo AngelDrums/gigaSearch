65defe2770b30986596bad9927d654c48ca8d1d6
EduardGodi <angelmaster.ag@gmail.com>
2024-07-15 13:29:45 -0600

    Primer avance

---
+++ js/users.js
@@ -0,0 +1,61 @@
+const users = [
+    // San Bernandino
+    {
+        nombre: 'Moises',
+        apellido: 'Quintero',
+        usuario: 'aguirrequinterogt',
+        pass: 'ADM',
+        ubicación: "San Bernandino"
+    },
+    {
+        nombre: 'Mikaela',
+        apellido: '',
+        usuario: 'mikailagt',
+        pass: 'GNC2',
+        ubicación: "San Bernandino"
+    },
+    {
+        nombre: 'Cevicheria el Pulpo',
+        apellido: '',
+        usuario: 'cevicheriaelpulpogt',
+        pass: 'GNC',
+        ubicación: "San Bernandino"
+    },
+    {
+        nombre: 'Santiago',
+        apellido: 'Toj',
+        usuario: 'santiagotoj',
+        pass: 'Excel',
+        ubicación: "San Bernandino"
+    },
+    {
+        nombre: 'Reyes',
+        apellido: 'Tolcha',
+        usuario: 'reyestolchagt',
+        pass: 'GNC',
+        ubicación: "San Bernandino"
+    },
+    //Madre Mia
+    {
+        nombre: 'Marisol',
+        apellido: 'Canil',
+        usuario: 'marisolcanilgt',
+        pass: 'Excel',
+        ubicación: "Madre mia"
+    },
+    {
+        nombre: 'Rene',
+        apellido: 'Coc',
+        usuario: 'renecocgt',
+        pass: 'GNC',
+        ubicación: "Madre mia"
+    },
+    // San Pablo
+    {
+        nombre: 'Héctor',
+        apellido: 'Cardenas',
+        usuario: 'hectorcardenasgt',
+        pass: 'GNC2',
+        ubicación: "San Pablo Jocopilas"
+    },
+]
